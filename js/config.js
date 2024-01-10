e6c174c3b1000ac8a630856af8277ceb5d65d867
Arnoud de Boer <a.deboer@geonovum.nl>
2024-01-10 15:44:50 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,11 +4,11 @@ let respecConfig = {
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Roadmap geo-standaarden",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  //specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
-  //specStatus: "basis",            // Basis Document
+  specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                 // Norm
   //specType: "ST",                 // Standaard
