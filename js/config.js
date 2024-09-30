ac01bd0f1cfcb033acb1bdedb1790df874ef7820
Wilko Quak <w.quak@geonovum.nl>
2024-09-30 11:20:31 +0200

    licence --> license

--- js/config.js
+++ js/config.js
@@ -22,9 +22,9 @@ let respecConfig = {
   //-- pubDomain is verplicht! (komt in de URL) -------------------------------------
   pubDomain: "rmgs",
   //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
-  //licence: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
-  //licence: "cc0",                 // Public Domain Dedication
-  licence: "cc-by",                 // Attribution, met bronvermelding
+  //license: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
+  //license: "cc0",                 // Public Domain Dedication
+  lisence: "cc-by",                 // Attribution, met bronvermelding
   //-- shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
   shortName: "rmgs",
   
